b9166ed76e83822c7cd05afa946478a782126db8
Venkatesh Paruchuri <venkatesh.paruchuri@thrymr.net>
2017-02-17 10:42:06 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+//Parallax 
+function simpleParallax() {
+    //This variable is storing the distance scrolled
+    var scrolled = $(window).scrollTop() + 1;
+
+    //Every element with the class "scroll" will have parallax background 
+    //Change the "0.3" for adjusting scroll speed.
+    $('.banner_image').css('background-position', '0' + -(scrolled * 0.3) + 'px');
+}
+//Everytime we scroll, it will fire the function
+$(window).scroll(function (e) {
+    simpleParallax();
+});
+$(document).ready(function(){
+	var height = $(window).height();
+	$(".banner_image").css("height",Number(height)+'px');
+	$(".navbar_section").css("top",Number(height)-120+'px');
+});
+
+$( document ).ready(function() {
+    $('.video_section').hover(
+        function(){
+            $(this).find('.caption').fadeIn(250);
+             $(this).find('.caption').css("padding-bottom","20px");//.fadeIn(250)
+        },
+        function(){
+            $(this).find('.caption').fadeOut(250); //.fadeOut(205)
+        }
+    ); 
+});
